2615231da9797538a5bdda45443908f86befb8da
Vedvardhan <vedvardhan.g@gmail.com>
2024-10-11 20:35:02 +0530

    Update layout.tsx

--- apps/landingpage-actions/src/app/layout.tsx
+++ apps/landingpage-actions/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import logo from "../assets/logo.svg"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
